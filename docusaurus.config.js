3b6812b973775f02071d4166b9287ce838daccf9
rushtehrani <r@onepanel.io>
2020-09-14 19:46:31 -0700

    update top menu item title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,8 +25,8 @@ module.exports = {
       },
       items: [
         {to: 'docs/getting-started/quickstart', label: 'Getting Started', position: 'left'},
-        {to: 'docs/deployment/overview', label: 'Deploy and Manage', position: 'left'},
         {to: 'docs/reference/overview', label: 'User Guide', position: 'left'},
+        {to: 'docs/deployment/overview', label: 'Operator Manual', position: 'left'},
         {to: 'docs/api-sdk/overview', label: 'APIs and SDKs', position: 'left'},
         {href: 'https://github.com/onepanelio/core/releases', label: 'Releases', position: 'left'},
         {href: 'https://github.com/onepanelio/core/milestones?direction=asc&sort=due_date&state=open', label: 'Roadmap', position: 'left'},
