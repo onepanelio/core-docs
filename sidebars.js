5b0e8a7bd4f77572948a8b4e56075f99d7d87031
rushtehrani <r@onepanel.io>
2021-04-02 09:13:29 -0700

    remove unused pages

--- sidebars.js
+++ sidebars.js
@@ -35,18 +35,6 @@ module.exports = {
       type: 'doc',
       id: 'deployment/overview',
     },
-    // { 
-    //   type: 'category',
-    //   label: 'Deployment',
-    //   collapsed: false,
-    //   items: [
-    //     'deployment/public/aks',
-    //     'deployment/public/eks',
-    //     'deployment/public/gke',
-    //     // 'deployment/single-node/microk8s',
-    //     // 'deployment/single-node/minikube',
-    //   ]
-    // },
     { 
       type: 'category',
       label: 'Configuration',
